f2f1c754d28412a0ade023f13053e08fb9a3dfab
davijose <davijose40@gmail.com>
2020-08-27 15:05:42 -0300

    create crud in ProductController

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -3,7 +3,7 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const Product = use('App/Models/Product')
 /**
  * Resourceful controller for interacting with products
  */
@@ -17,7 +17,18 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const name = request.input('name')
+
+    const query = Product.query()
+
+    if (name) {
+      query.where('name', 'ILIKE', `%${name}%`)
+    }
+
+    const products = await query.pagination(pagination.page, pagination.limit)
+    return response.send(products)
+  }
 
   /**
    * Create/save a new product.
@@ -27,18 +38,35 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      const { name, image_id, description, price } = request.all()
+
+      const product = await Product.create({
+        name,
+        image_id,
+        description,
+        price,
+      })
+      return response.status(201).send(product)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Erro ao cadastrar produto',
+      })
+    }
+  }
 
   /**
    * Display a single product.
    * GET products/:id
    *
    * @param {object} ctx
-   * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, response }) {
+    const product = await Product.findOrFail(id)
+    return response.send(product)
+  }
 
   /**
    * Update product details.
@@ -48,7 +76,19 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params, request, response }) {
+    const product = await Product.findOrFail(id)
+    try {
+      const { name, description, price, image_id } = request.all()
+      product.merge({ name, description, price, image_id })
+      await product.save()
+      return response.send(product)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível atualizar o produto nesse momento.',
+      })
+    }
+  }
 
   /**
    * Delete a product with id.
@@ -58,7 +98,17 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const product = await Product.findOrFail(id)
+    try {
+      await product.destroy()
+      return response.status(204).send()
+    } catch (error) {
+      return response
+        .status(500)
+        .send({ message: 'Erro ao deletar o produto.' })
+    }
+  }
 }
 
 module.exports = ProductController
